570b47c297a4856918bbdaebdfc1a5be3fcc5f6a
mikadominguezz <mikadominguezz2004@gmail.com>
2024-10-19 03:21:30 -0300

    ediciones home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -94,75 +94,83 @@ const Home = () => {
             </div>
             <h1 style={{ color: "white", margin: "0"}}>Welcome to Gaming Beyond Earth!</h1>
             <p style={{ color: "white", fontSize: "600"}}>Pick your Game Mode</p>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "18px", marginTop: "40px"  }}>
-                <button
-                    onClick={handleStartReflexGame}
-                    style={{
-                        padding: "10px 20px",
-                        fontSize: "18px",
-                        cursor: "pointer",
-                        backgroundColor: "#7cd6bb",
-                        color: "white",
-                        border: "none",
-                        borderRadius: "5px",
-                        borderBottom: "3px solid #5a8c7d",
-                        borderLeft: "3px solid #5a8c7d"
-                    }}
-                >
-                    Play Reflex Mode
-                </button>
 
-                <button
-                    onClick={handleStartMemoryGame}
-                    style={{
-                        padding: "10px 20px",
-                        fontSize: "18px",
-                        cursor: "pointer",
-                        backgroundColor: "#7cd6bb",
-                        color: "white",
-                        border: "none",
-                        borderRadius: "5px",
-                        borderBottom: "3px solid #5a8c7d",
-                        borderLeft: "3px solid #5a8c7d"
-                    }}
-                >
-                    Play Memory Mode
-                </button>
+            <div style={{display:"flex", justifyContent:"space-evenly", marginBottom:"20px"}}>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "18px", marginTop: "40px"}}>
+                    <button
+                        onClick={handleStartReflexGame}
+                        style={{
+                            padding: "10px 20px",
+                            fontSize: "18px",
+                            cursor: "pointer",
+                            backgroundColor: "#7cd6bb",
+                            color: "white",
+                            border: "none",
+                            borderRadius: "5px",
+                            borderBottom: "3px solid #5a8c7d",
+                            borderLeft: "3px solid #5a8c7d"
+                        }}
+                    >
+                        Play Reflex Mode
+                    </button>
 
-                <button
-                    onClick={handleStartLearningGame}
-                    style={{
-                        padding: "10px 20px",
-                        fontSize: "18px",
-                        cursor: "pointer",
-                        backgroundColor: "#7cd6bb",
-                        color: "white",
-                        border: "none",
-                        borderRadius: "5px",
-                        borderBottom: "3px solid #5a8c7d",
-                        borderLeft: "3px solid #5a8c7d"
-                    }}
-                >
-                    Play Learning Mode
-                </button>
+                    <button
+                        onClick={handleStartMemoryGame}
+                        style={{
+                            padding: "10px 20px",
+                            fontSize: "18px",
+                            cursor: "pointer",
+                            backgroundColor: "#7cd6bb",
+                            color: "white",
+                            border: "none",
+                            borderRadius: "5px",
+                            borderBottom: "3px solid #5a8c7d",
+                            borderLeft: "3px solid #5a8c7d"
+                        }}
+                    >
+                        Play Memory Mode
+                    </button>
+                    </div>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "18px", marginTop: "40px"}}>
+                    <button
+                        onClick={handleStartLearningGame}
+                        style={{
+                            padding: "10px 20px",
+                            fontSize: "18px",
+                            cursor: "pointer",
+                            backgroundColor: "#7cd6bb",
+                            color: "white",
+                            border: "none",
+                            borderRadius: "5px",
+                            borderBottom: "3px solid #5a8c7d",
+                            borderLeft: "3px solid #5a8c7d"
+                        }}
+                    >
+                        Play Learning Mode
+                    </button>
 
-                <button
-                    onClick={handleStartMusicalGame}
-                    style={{
-                        padding: "10px 20px",
-                        fontSize: "18px",
-                        cursor: "pointer",
-                        backgroundColor: "#7cd6bb",
-                        color: "white",
-                        border: "none",
-                        borderRadius: "5px",
-                        borderBottom: "3px solid #5a8c7d",
-                        borderLeft: "3px solid #5a8c7d"
-                    }}
-                >
-                    Play Musical Mode
-                </button>
+                    <button
+                        onClick={handleStartMusicalGame}
+                        style={{
+                            padding: "10px 20px",
+                            fontSize: "18px",
+                            cursor: "pointer",
+                            backgroundColor: "#7cd6bb",
+                            color: "white",
+                            border: "none",
+                            borderRadius: "5px",
+                            borderBottom: "3px solid #5a8c7d",
+                            borderLeft: "3px solid #5a8c7d"
+                        }}
+                    >
+                        Play Musical Mode
+                    </button>
+                </div>
+            </div>
+            
 
+
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "18px", marginTop: "40px"}}>
                 <button
                     onClick={goToRanking}
                     style={{
